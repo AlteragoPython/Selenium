925d285e434fd86bc98ac9c8c4d12b429cd20ad6
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-06-04 22:12:30 +0300

    add api test2 for delete metod

--- API.py
+++ API.py
@@ -8,3 +8,10 @@ def test_response_content():
     assert beer_data["abv"] == 4.7, f"Expected abv 4.7, but got {beer_data['abv']}"
     assert response.status_code == 200, f"Expected status code 200, but got {response.status_code}"
 
+def test_delete_status_code():
+    url = 'https://api.punkapi.com/v2/beers/8'
+    response = requests.delete(url)
+    assert response.status_code == 404, f"Expected status code 404, but got {response.status_code}"
+    response_data = response.json()
+    expected_message = 'No endpoint found that matches \'/v2/beers/8\''
+    assert response_data["message"] == expected_message, f"Expected message '{expected_message}', but got {response_data['message']}"
