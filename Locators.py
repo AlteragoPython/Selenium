f6b5a2b3c3ff649d105fd04cff36cb996265fb13
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-06-04 16:47:56 +0300

    add class base_page

--- Locators.py
+++ Locators.py
@@ -1,4 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class Base_page:
+    driver = webdriver.Chrome()
+    url = 'https://www.saucedemo.com/'
+    driver.get(url)
+
+
+
 class Locators:
-    Username_field = (By.name,'user-name')
-    Pasword_field = (By.XPath,'//*[@id="password"]')
-    Login_button = (By.Id,'login-button')
+    Username_field = (By.NAME,'user-name')
+    Password_field = (By.XPATH,'//*[@id="password"]')
+    Login_button = (By.ID,'login-button')
+
