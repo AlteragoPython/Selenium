7fb20230fee580f3751872bcf1cb9f7ee438b5a9
Mikhail_Chizhov <mikhail.chizhov.test@gmail.com>
2023-06-04 18:10:37 +0300

    add set password and login check expected url

--- HW13.py
+++ HW13.py
@@ -1,9 +1,24 @@
 from selenium import webdriver
 import Locators
 
-driver = webdriver.Chrome()   # create instance of the driver
-driver.get("https://www.saucedemo.com/") # go to the website
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
 
-username_field = driver.find_element(*Locators.Locators.Username_field) # find element username
+# find element username
+username_field = driver.find_element(*Locators.Locators.Username_field)
 username_field.clear()
 username_field.send_keys("standard_user")
+
+# find element password
+password_field = driver.find_element(*Locators.Locators.Password_field)
+password_field.clear()
+password_field.send_keys("secret_sauce")
+
+# login to the website
+login_button = driver.find_element(*Locators.Locators.Login_button)
+login_button.click()
+
+# check  expected URL
+current_url = driver.current_url
+expected_url = "https://www.saucedemo.com/inventory.html"
+assert current_url == expected_url
